bca63e005674fb6a6920b565df1e6985820e4f33
Carlos Escobar <escobarcarlos@gmail.com>
2022-12-12 22:05:51 -0800

    info-flex

--- src/Components/InfoSection/Info.tsx
+++ src/Components/InfoSection/Info.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Icon } from "@iconify/react";
 
 const Info: React.FC = () => {
   return (
@@ -19,18 +20,31 @@ const Info: React.FC = () => {
             works properly.
           </p>
         </div>
+        <div className="w-120 h-40 bg-white grid grid-cols-1">
+          <div className="w-120 h-40">Box 1</div>
+          <div className=" w-120 h-40 grid grid-cols-2 ">
+            <div className=" flex flex-row w-full h-full justify-center items-center bg-black">
+              <Icon
+                icon="icon-park-outline:facebook-one"
+                fontSize="60px"
+                color="white"
+                className="mt-10 ml-10"
+              />
+              <p className="text-center text-white">J&FTrailerRentals</p>
+            </div>
+            <div className="w-full h-full bg-black">
+              Jftrailerrentals@gmail.com (209)985-5943
+            </div>
+          </div>
+        </div>
 
-        <div className="justify-center items-center  ">
+        {/* <div className="justify-center items-center  ">
           <div className="w-120 h-40 bg-white flex items-center justify-center">
             <p className="text-center text-xl font-semibold">
               Feel free, to message me the Pick up/drop off Adresses for a quote
             </p>
           </div>
-          <div className=" grid grid-cols-1">
-            <div className="w-120 h-40 bg-black outline"></div>
-            <div className="w-120 h-40 bg-black outline"></div>
-          </div>
-        </div>
+        </div> */}
 
         {/* <div className="justify-center items-center">
           <div className="w-120 h-40 bg- outline">
